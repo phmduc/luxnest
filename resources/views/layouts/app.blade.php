d138373f04e5f59d058715a7c048edcf765cdbc8
phmduc1012 <phmduc1012@gmail.com>
2026-07-01 22:56:23 +0700

    Add og:image:width, height, type, secure_url for Zalo/Facebook crawler

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,12 @@
     <meta property="og:title"       content="@yield('og_title', $defaultOgTitle)">
     <meta property="og:description" content="@yield('og_description', $defaultOgDesc)">
     <meta property="og:url"         content="{{ url()->current() }}">
-    <meta property="og:image"       content="@yield('og_image', $defaultOgImage)">
-    <meta property="og:locale"      content="vi_VN">
+    <meta property="og:image"            content="@yield('og_image', $defaultOgImage)">
+    <meta property="og:image:secure_url" content="@yield('og_image', $defaultOgImage)">
+    <meta property="og:image:type"       content="image/jpeg">
+    <meta property="og:image:width"      content="1200">
+    <meta property="og:image:height"     content="630">
+    <meta property="og:locale"           content="vi_VN">
 
     <!-- Twitter Card -->
     <meta name="twitter:card"        content="summary_large_image">
